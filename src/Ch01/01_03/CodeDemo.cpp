05c81f9aad6589bf0c84615d7cb723c89103ad8e
RanjulaHettiarachchi <61304398+RanjulaHettiarachchi@users.noreply.github.com>
2024-10-28 13:20:15 +0000

    CH1_03

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -6,9 +6,10 @@
 #include <string>
 
 int main(){
+    std::cout << "Insert Your Name: " << std::endl;
     std::string str;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Hello!" << str << " How are You? " << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
